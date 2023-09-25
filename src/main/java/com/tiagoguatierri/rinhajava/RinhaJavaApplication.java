ce784ac00afa91cbc1856237db260870ebf1fb3d
Tiago Guatierri <tiagovit@gmail.com>
2023-09-25 08:44:14 -0300

    feat: app skeleton

---
+++ src/main/java/com/tiagoguatierri/rinhajava/RinhaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoguatierri.rinhajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagoguatierri/rinhajava/RinhaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoguatierri.rinhajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
